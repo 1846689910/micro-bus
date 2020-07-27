221debe9bf49d375711b4240188505e451c6a761
1846689910 <eric14706@gmail.com>
2020-07-27 15:36:22 -0700

    mod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,39 @@
 import React from "react";
+import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
+import Title from "../ts/home/Title";
+
+const useStyles = makeStyles({
+  outer: {
+    width: "100%",
+    height: "90vh",
+    // border: "1px solid black",
+    margin: 0,
+    padding: 0,
+  },
+});
+
 export default function Index() {
-  return <h1>Welcome to micro-service!</h1>;
+  const classes = useStyles();
+  return (
+    <Container maxWidth="md" style={{ padding: 0 }}>
+      <Grid
+        className={classes.outer}
+        container
+        alignItems="center"
+        justify="center"
+      >
+        <Grid
+          item
+          container
+          direction="column"
+          style={{ height: "60vh", border: "1px solid black" }}
+        >
+          <Title />
+          <Grid container justify="center">
+            Table
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
