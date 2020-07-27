7d83f726f78a54dcd3e68c4fd67ed08daa27b5db
1846689910 <eric14706@gmail.com>
2020-07-27 16:07:47 -0700

    mod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
+import { Container, Grid, makeStyles } from "@material-ui/core";
 import Title from "../ts/home/Title";
 import APITable from "../ts/home/APITable";
 
