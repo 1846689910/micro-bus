aa70823b8870d851ddcad6457bb9016adf494a26
1846689910 <eric14706@gmail.com>
2020-07-29 00:20:03 -0700

    set redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import App from "next/app";
+import { Provider } from "react-redux";
 import { MediaQueryProvider } from "../ts/MediaQueryContext";
+import { configureStore } from "../server/settings/store";
 
 export default class _App extends App {
   render() {
+    const store = configureStore();
     const { Component, pageProps } = this.props;
     return (
-      <MediaQueryProvider>
-        <Component {...pageProps} />
-      </MediaQueryProvider>
+      <Provider store={store}>
+        <MediaQueryProvider>
+          <Component {...pageProps} />
+        </MediaQueryProvider>
+      </Provider>
     );
   }
 }
