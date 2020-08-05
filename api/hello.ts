8f7f77b125fa2e1902fffde17881d9cd6ea6948b
1846689910 <eric14706@gmail.com>
2020-08-05 13:49:56 -0700

    mod

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,9 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 
 export default (request: NowRequest, response: NowResponse) => {
-  console.log(request);
-  console.log(response);
-  const { name = "World" } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+  const obj = {
+    request,
+    response,
+  };
+  response.status(200).send(JSON.stringify(obj, null, 2));
 };
