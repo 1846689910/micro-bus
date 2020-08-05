8e49a9a57a25ed30457ec267572c6bc2678a6073
1846689910 <eric14706@gmail.com>
2020-08-05 13:35:07 -0700

    mod

--- api/type-18-hek.ts
+++ api/type-18-hek.ts
@@ -12,4 +12,13 @@ export default async (req: NowRequest, res: NowResponse) => {
   const body = req.method === "POST" ? await json(req) : {};
   console.log(`graphql body = ${JSON.stringify(body, null, 2)}`);
   await handler(req, res);
+  return {
+    props: {
+      host: req.host || "",
+      method: req.method,
+      url: req.url,
+      timestamp: Date.now(),
+      body,
+    }, // will be passed to the page component as props
+  };
 };
