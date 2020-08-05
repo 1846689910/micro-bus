fc5778b31b73c3958b1aa32b4c2384521f394ee6
1846689910 <eric14706@gmail.com>
2020-08-05 13:30:16 -0700

    mod

---
+++ api/type-18-hek.ts
@@ -0,0 +1,15 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import { json, send } from "micro";
+import { OK } from "http-status";
+import { handler } from "../src/server/type-18-hek/graphql";
+import { setAllowCorsHeaders } from "../src/server/utils";
+
+export default async (req: NowRequest, res: NowResponse) => {
+  setAllowCorsHeaders(res);
+  if (req.method === "OPTIONS") {
+    return send(res, OK);
+  }
+  const body = req.method === "POST" ? await json(req) : {};
+  console.log(`graphql body = ${JSON.stringify(body, null, 2)}`);
+  await handler(req, res);
+};
