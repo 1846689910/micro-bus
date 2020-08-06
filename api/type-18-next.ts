3484ec2f4dee3dfd4ab1f0a3a8ac503f658f8d70
1846689910 <eric14706@gmail.com>
2020-08-05 17:41:11 -0700

    mod

--- api/type-18-next.ts
+++ api/type-18-next.ts
@@ -1,7 +1,7 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 import { OK } from "http-status";
 import { setAllowCorsHeaders } from "../src/server/utils";
-import { handler } from "../src/server/type-18-hek/graphql";
+import { handler } from "../src/server/type-18-next/graphql";
 
 export default async (request: NowRequest, response: NowResponse) => {
   const { method } = request;
