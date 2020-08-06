b35d1412c057244c11d1b2b81d057464e4154587
1846689910 <eric14706@gmail.com>
2020-08-05 19:51:01 -0700

    mod

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ module.exports = {
   exportPathMap: async () => {
     return {
       "/": { page: "/" },
-      // "/_api/type-18-hek": { page: "/_api/type-18-hek" },
-      // "/_api/type-18-next": { page: "/_api/type-18-next" },
     };
   },
 };
